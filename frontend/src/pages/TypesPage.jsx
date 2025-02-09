6e789a47ecdaba4d54e473432623d51b7bcdf476
briannavo <briannavo2016@gmail.com>
2025-02-08 22:22:05 -0500

    finished types page

--- frontend/src/pages/TypesPage.jsx
+++ frontend/src/pages/TypesPage.jsx
@@ -3,9 +3,11 @@ import Tabs from '../components/Tabs'
 
 const TypesPage = () => {
   return (
-    <div>
-      <Tabs /> 
-    </div>
+    <><div style={{backgroundColor: 'lightblue', padding:'10px', textAlign:'center', fontSize:'25px'}}>
+      <h1>Remember, you are not alone. Nearly 25% of all women experience domestic violence at some point in their lives. Learn more about what abuse can look like below.</h1>
+    </div><div>
+        <Tabs />
+      </div></>
   )
 }
 
